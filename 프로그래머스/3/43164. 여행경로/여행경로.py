9e456700aade3577842e5296f418ee36b25ccbd2
NAEUN KIM <79038393+naeunkim1@users.noreply.github.com>
2024-02-19 22:26:05 +0900

    [level 3] Title: 여행경로, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/43164. 여행경로/여행경로.py
@@ -0,0 +1,18 @@
+from collections import deque
+def solution(tickets):
+    answer = []
+    q = deque()
+    q.append(("ICN", ["ICN"], []))
+    print(q)
+    
+    while q:
+        airport, path, used = q.popleft()
+        if len(used) == len(tickets):
+            answer.append(path)
+
+        for idx, ticket in enumerate(tickets):
+            if ticket[0] == airport and idx not in used:
+                q.append((ticket[1], path+[ticket[1]], used+[idx]))
+                
+    answer.sort()
+    return answer[0]
