9649aeb83c2acf75e702a8c4f5cfa3f96ac47130
NAEUN KIM <79038393+naeunkim1@users.noreply.github.com>
2024-03-21 23:10:53 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.04 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
@@ -1,4 +1,3 @@
 def solution(s):
-    answer = list(map(int, s.split(' ')))
-    answer.sort()
-    return str(answer[0]) + ' ' + str(answer[-1])
+    lst = list(map(int, s.split()))
+    return str(min(lst))+" "+str(max(lst))
